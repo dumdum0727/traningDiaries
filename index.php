3438061baa0f6a0541904e325a89f597e700a223
dumdum0727 <sekimen0727@gmail.com>
2022-02-27 14:18:54 +0900

    ホームの大枠できた

--- index.php
+++ index.php
@@ -5,8 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>筋トレ日記</title>
-</head>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet">
 <body>
+    <header>
+        <h1>きんとれにっき</h1>
+        <p>～トレーニーの匿名掲示板～</p>
+    </header>
+    <div class="contents_area">
+        <div class="right_side">
     
+        </div>
+        <div class="main">
+        <button class="Button-style">投稿する</button>
+        
+        </div>
+        <div class="left_side">
+    
+        </div>
+    </div>
+
 </body>
 </html>
